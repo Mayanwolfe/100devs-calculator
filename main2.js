2a685bc06afd655afc1370a7f0135f4c1c712526
Mayanwolfe <cebourdon625@gmail.com>
2022-04-02 18:41:33 -0400

    made methods more dry, added better parsing

--- main2.js
+++ main2.js
@@ -12,39 +12,56 @@ const keys = document.querySelector('.calculator-keys');
 
 const calculator = {
     displayText: '0',
+    prevTotal: null,
 
     parseInput(value) {
         if (this.displayText === '0') {
             this.displayText = ''
         } 
-        if (value === '=') {
-            this.calcAnswer(this.displayText)
-        } else if (value === 'AC') {
-            this.clearAll()
-        } else if (isNaN(this.displayText.charAt(this.displayText.length - 1))  && isNaN(+(value))) {
-            return;
-        } else if (value === '.' ) {
-            if (this.displayText === '') {
-                this.addText('0' + value)
-            } else {
+        switch (value) {
+            case '=' :
+                this.calcAnswer(this.displayText)
+                break;
+            case 'AC' :
+                this.clearAll()
+                break;
+            case '.' :
+                if (this.displayText == 0) {
+                    this.addText('0.')
+                } else {
+                    this.addText(value)
+                }
+                break;
+            default:
                 this.addText(value)
-            }
-        } else {
-            this.addText(value)
+            
         }
     },
 
     addText(value) {
+        if (this.displayText === '0') {
+            this.displayText = ''
+        } else if (this.prevTotal !== null) {
+            this.displayText = this.prevTotal
+            this.prevTotal = null
+        }
+        if (isNaN(+(value)) && isNaN(this.displayText)) {
+            if(isNaN(this.displayText.slice(-1))) {
+                return;
+            }
+        }
         this.displayText += value
         this.outputText( this.displayText)
     },
 
     calcAnswer(equation) {
         !(this.displayText == 0) && this.outputText( eval(equation))
+        this.prevTotal = eval(equation)
     },
 
     clearAll() {
         this.displayText = '0',
+        this.prevTotal = null,
         this.outputText(this.displayText)
     },
 

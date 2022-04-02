0150c06443cd977be3f76dc3086bf697f88e6002
Mayanwolfe <cebourdon625@gmail.com>
2022-04-02 19:24:57 -0400

    cleaned up JS files

--- main.js
+++ main.js
@@ -1,111 +1,75 @@
-const calculator = {
-    displayValue: '0',
-    firstOperand: null,
-    waitingForSecondOperand: false,
-    operator: null,
-  };
-  
-  function inputDigit(digit) {
-    const { displayValue, waitingForSecondOperand } = calculator;
-  
-    if (waitingForSecondOperand === true) {
-      calculator.displayValue = digit;
-      calculator.waitingForSecondOperand = false;
-    } else {
-      calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-    }
-  }
-  
-  function inputDecimal(dot) {
-    if (calculator.waitingForSecondOperand === true) {
-        calculator.displayValue = "0."
-      calculator.waitingForSecondOperand = false;
-      return
-    }
-  
-    if (!calculator.displayValue.includes(dot)) {
-      calculator.displayValue += dot;
-    }
-  }
-  
-  function handleOperator(nextOperator) {
-    const { firstOperand, displayValue, operator } = calculator
-    const inputValue = parseFloat(displayValue);
-    
-    if (operator && calculator.waitingForSecondOperand)  {
-      calculator.operator = nextOperator;
-      return;
-    }
-  
-  
-    if (firstOperand == null && !isNaN(inputValue)) {
-      calculator.firstOperand = inputValue;
-    } else if (operator) {
-      const result = calculate(firstOperand, inputValue, operator);
-  
-      calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
-      calculator.firstOperand = result;
-    }
-  
-    calculator.waitingForSecondOperand = true;
-    calculator.operator = nextOperator;
-  }
-  
-  function calculate(firstOperand, secondOperand, operator) {
-    if (operator === '+') {
-      return firstOperand + secondOperand;
-    } else if (operator === '-') {
-      return firstOperand - secondOperand;
-    } else if (operator === '*') {
-      return firstOperand * secondOperand;
-    } else if (operator === '/') {
-      return firstOperand / secondOperand;
-    }
-  
-    return secondOperand;
-  }
-  
-  function resetCalculator() {
-    calculator.displayValue = '0';
-    calculator.firstOperand = null;
-    calculator.waitingForSecondOperand = false;
-    calculator.operator = null;
-  }
-  
-  function updateDisplay() {
-    const display = document.querySelector('.calculator-screen');
-    display.value = calculator.displayValue;
-  }
-  
-  updateDisplay();
-  
-  const keys = document.querySelector('.calculator-keys');
+const keys = document.querySelector('.calculator-keys');
   keys.addEventListener('click', event => {
     const { target } = event;
     const { value } = target;
     if (!target.matches('button')) {
       return;
+    } else {
+        calculator.parseInput(value)
     }
-  
-    switch (value) {
-      case '+':
-      case '-':
-      case '*':
-      case '/':
-      case '=':
-        handleOperator(value);
-        break;
-      case '.':
-        inputDecimal(value);
-        break;
-      case 'all-clear':
-        resetCalculator();
-        break;
-      default:
-        if (Number.isInteger(parseFloat(value))) {
-          inputDigit(value);
+
+})
+
+const calculator = {
+    displayText: '0',
+    prevTotal: null,
+
+    parseInput(value) {
+        if (this.displayText === '0') {
+            this.displayText = ''
+        } 
+        switch (value) {
+            case '=' :
+                this.calcAnswer(this.displayText)
+                break;
+            case 'AC' :
+                this.clearAll()
+                break;
+            case '.' :
+                if (this.displayText == 0) {
+                    this.addText('0.')
+                } else {
+                    this.addText(value)
+                }
+                break;
+            default:
+                this.addText(value)
+            
         }
+    },
+
+    addText(value) {
+        if (this.displayText === '0') {
+            this.displayText = ''
+        } else if (this.prevTotal !== null) {
+            this.displayText = this.prevTotal
+            this.prevTotal = null
+        }
+        if (isNaN(+(value)) && isNaN(this.displayText)) {
+            if(isNaN(this.displayText.slice(-1))) {
+                return;
+            }
+        }
+        this.displayText += value
+        this.outputText( this.displayText)
+    },
+
+    calcAnswer(equation) {
+        let result = Function("return " + equation)()
+        !(this.displayText == 0) && this.outputText( result)
+        this.prevTotal = result
+    },
+
+    clearAll() {
+        this.displayText = '0',
+        this.prevTotal = null,
+        this.outputText(this.displayText)
+    },
+
+    outputText(text) {
+        document.querySelector('.calculator-screen').value = text
     }
-  
-    updateDisplay();
-  });
+
+  };
+
+
